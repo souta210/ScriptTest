6be0f47a3b9c597d52cfb6cdcad64a405e4b49ae
koike souta <kaomoziorize@gmail.com>
2017-03-18 17:15:41 +0900

    Lesson4課題提出用

---
+++ Assets/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+
+	// Use this for initialization
+		
+	void Start ()
+	{
+		int[] array = {5,8,14,56,79};
+		for (int a=0;a<5;a++){
+			Debug.Log (array[a]);
+		}
+			for (int b = 4; b > 0; b--) {
+				Debug.Log (array [b]); 
+			}
+
+				
+	}
+
+	// Update is called once per frame
+void Update ( ) {
+		}
+ }
---
+++ Assets/test2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss {
+	private int mp = 53;
+	private int syouhi = 5;
+
+	public void Magic(){
+		if(mp > 5){
+			int nokori = mp - syouhi;
+			Debug.Log("魔法攻撃をした。残りMPは" +nokori+"。");
+			mp = nokori;
+		} else {
+			Debug.Log ("MPが足りないため魔法が使えない。");
+		}
+	}
+
+}
+
+public class test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Boss lastBoss = new Boss ();
+		for (int i = 0; i < 11; i++) {
+			lastBoss.Magic ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
